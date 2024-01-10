c71ebf4ee072a65d618e1732640b5fe833e7ae81
Familysga <134228968+Familysga@users.noreply.github.com>
2024-01-10 22:41:13 +0300

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,15 +57,15 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("zzSvv")
-                await self.one.join_chat("zzsvv")
-                await self.one.join_chat("zzsvv")
+                await self.one.join_chat("vrrrrvr")
+                await self.one.join_chat("F6FF6")
+                await self.one.join_chat("T9T99T")
             except:
                 pass
             assistants.append(1)
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, "بدء"
+                    config.LOG_GROUP_ID, "Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
@@ -88,15 +88,15 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("zzsvv")
-                await self.two.join_chat("zzsvv")
-                await self.two.join_chat("zzsvv")
+                await self.two.join_chat("vrrrrvr")
+                await self.two.join_chat("F6FF6")
+                await self.two.join_chat("T9T99T")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, "بداء"
+                    config.LOG_GROUP_ID, "Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
@@ -119,15 +119,15 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("zzsvv")
-                await self.three.join_chat("zzsvv")
-                await self.three.join_chat("zzsvv")
+                await self.three.join_chat("vrrrrvr")
+                await self.three.join_chat("F6FF6")
+                await self.three.join_chat("T9T99T")
             except:
                 pass
             assistants.append(3)
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, "بداء"
+                    config.LOG_GROUP_ID, "Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
@@ -150,9 +150,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("sspaa")
-                await self.four.join_chat("sspaa")
-                await self.four.join_chat("sspaa")
+                await self.four.join_chat("vrrrrvr")
+                await self.four.join_chat("F6FF6")
+                await self.four.join_chat("T9T99T")
             except:
                 pass
             assistants.append(4)
@@ -181,9 +181,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("sspaa")
-                await self.five.join_chat("sspaa")
-                await self.five.join_chat("sspaa")
+                await self.five.join_chat("vrrrrvr")
+                await self.five.join_chat("F6FF6")
+                await self.five.join_chat("T9T99T")
             except:
                 pass
             assistants.append(5)
